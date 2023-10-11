3a499eb82efd0667a4c89ca5669bdcef62f70faf
Javier Bullrich <javier@bullrich.dev>
2023-10-11 15:05:18 +0200

    added connection between events

--- src/action.ts
+++ src/action.ts
@@ -1,9 +1,34 @@
-import { context, getOctokit } from "@actions/github";
+import { getInput } from "@actions/core";
+import { context, getOctokit, } from "@actions/github";
 
-console.log("Hello via Bun!");
+const token = getInput("TOKEN", {required:true});
+
+const name = getInput("NAME");
+
+console.log("Hello via Bun!", "Action created by", name);
 
 console.log("eventName", context.eventName);
 
 console.log("payload", context.payload);
 
-console.log("context", context);
+// console.log("context", context);
+
+const api = getOctokit(token);
+
+const allArtifacts = await api.rest.actions.listWorkflowRunArtifacts({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.payload.workflow_run.id,
+});
+
+let matchArtifact = allArtifacts.data.artifacts.filter((artifact) => {
+    return artifact.name == "pr_number"
+  })[0];
+  let download = await api.rest.actions.downloadArtifact({
+     owner: context.repo.owner,
+     repo: context.repo.repo,
+     artifact_id: matchArtifact.id,
+     archive_format: 'zip',
+  });
+
+  console.log(download);
